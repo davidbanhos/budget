03bc0a2e2fe90358ecf4379af0333dc30eb1a340
David Saggese Banhos <david.banhos@gmail.com>
2017-03-11 18:45:04 +0800

    Added filter, both sintaxes

--- lib/budget.ex
+++ lib/budget.ex
@@ -4,10 +4,17 @@ defmodule Budget do
   def list_transactions do
     File.read!("lib/transactions-jan.csv")
     |> parse
+    |> filter
   end
 
   defp parse(string) do
     CSV.parse_string(string)
+  end
+
+  defp filter(rows) do
+    #Long sintaxe
+    #Enum.map(rows, fn(row) -> Enum.drop(row , 1) end)
+    Enum.map(rows, &Enum.drop(&1, 1))
   end
 
 end
